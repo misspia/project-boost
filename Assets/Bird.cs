7920d84784f8e0f60f491faf7b7ceb8f7a6d14d6
misspia <pia_leung@hotmail.com>
2018-01-03 22:07:41 -0800

    thrust factors, test object tagging

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+    [SerializeField] float thurstPower = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 	// Use this for initialization
@@ -14,30 +17,54 @@ public class Bird : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
-
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKey(KeyCode.Space))
+        switch(collision.gameObject.tag)
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            case "Friendly":
+                print("OK");
+                break;
+            case "Points":
+                print("Earned points");
+                break;
+            default:
+                print("dead");
+                // kill player
+                break;
+        }
+    }
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
-            } 
+            }
         }
         else
         {
             audioSource.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        float rotationThisFrame = thurstPower * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
-    }
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }    
 }
