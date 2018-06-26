a9642fc57a987647b3b3e648b740c8ab0e79b12f
misspia <pia_leung@hotmail.com>
2018-06-25 22:11:53 -0400

    96 test collisions

--- argon-assault/Assets/Scripts/Player.cs
+++ argon-assault/Assets/Scripts/Player.cs
@@ -22,7 +22,14 @@ public class Player : MonoBehaviour
     {
 
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Player collided something");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Player triggered something");
+    }
     // Update is called once per frame
     void Update()
     {
