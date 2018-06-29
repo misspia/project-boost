bb79ea191fc93ab23e9379eee13ad4ea45a48add
misspia <pia_leung@hotmail.com>
2018-06-28 22:17:12 -0400

    98 enable gameobject from collision handler

--- argon-assault/Assets/Scripts/CollisionHandler.cs
+++ argon-assault/Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class CollisionHandler : MonoBehaviour {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
 
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
     private void StartDeathSequence()
     {
-        print("Player dying");
         SendMessage("OnPlayerDeath");
 
+    }
+    private void ReloadScene() // string referenced
+    {
+        SceneManager.LoadScene(1);
     }
 }
